64f2f2109d2def00cfce12390f0d2468905ed09e
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2026-06-13 20:34:08 +0300

    remove icon

--- src/components/chat-sidebar.tsx
+++ src/components/chat-sidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a24899009e72c9ddb9e9f9efe3f3d538ad145365aba85ab777bf5c94df07394
-size 44038
+oid sha256:1fc4a6abe58d747dd18644e6c7181520abb5716c4e65d4f46f9e66e416d41b23
+size 43830
