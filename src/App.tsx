4cbdd8a0bd7c35e41e48ec3c05208e0e07322757
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2026-06-22 02:03:44 +0300

    fix minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c5eda9e69a98085ebfdfdeea5ca082fd6e37918084c31490cbbd7ac836d8fd9
-size 121871
+oid sha256:d336611e71bcf1d0da469cb380f0c12a8e6664bcbc0856779ee626fdca6fce2e
+size 122077
