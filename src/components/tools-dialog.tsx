d818d70d306a4350dabf4649e3d546d6a41bf449
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2026-07-01 01:55:05 +0300

    minor fix

--- src/components/tools-dialog.tsx
+++ src/components/tools-dialog.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c935cfa585ea0062089b33e3b3fa9319e61afddebff1b9e2eff530d4fb0981c
-size 106350
+oid sha256:feb507643792903c7d12d1a8fcac2dc3cf3933dcf34ceda9a0b8d6215d3ff2cf
+size 103618
